17f21210a6e832dd8558205096c50c48cd86c6d1
rioperezg <113980673+rioperezg@users.noreply.github.com>
2022-12-05 12:40:31 +0100

    es asiii

---
+++ Prueba.juego_La piedra.py
@@ -0,0 +1,27 @@
+def gameOfStones(n):
+    while True:
+        Num_piedras = print(input("Jugador1, Selecciones el numero de piedras a eliminar:"))
+        k = sum(Num_piedras)
+        k1 = sum(Num_piedras, Num_piedras1)
+        if  (n-k) != 8:
+            Num_piedras1 = print(input("Jugador2, Selecciones el numero de piedras a eliminar:"))
+        else:
+            print("Jugador 1 ha ganado")
+            break
+        if (n - k1) != 8:
+           Num_piedras
+        else:
+            print("Jugador 2 ha ganado")
+            break
+print(gameOfStones(12))
+
+            
+
+
+
+                
+
+
+
+    
+    
