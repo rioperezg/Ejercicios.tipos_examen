b99321072ec7c9379187047956cc8caea13ffc85
rioperezg <113980673+rioperezg@users.noreply.github.com>
2022-12-19 23:35:19 +0100

    40th commit

--- prueba.py
+++ prueba.py
@@ -1,5 +1,7 @@
-def Hollow_triangle(n):
-    for i in range(1, n+1):
-        print(" "*(n-i) + "#" + i*" " + "#" + " "*(n-i))
-    return None
-print(Hollow_triangle(6))
+def lunes(cumpleaños, fecha_actual):
+    Edad = fecha_actual[0] - cumpleaños[0]
+    if Edad >= 22 and Edad <= 78:
+        return (Edad*365)/7
+    else:
+        return 0
+print(lunes([1980, 4, 8], [2014, 6, 19]))
